c56d9e152f9938c08dff5c5faafefaf5347edac4
Ton Hoang <68850789+tonhoang@users.noreply.github.com>
2023-06-10 00:16:13 +0700

    Update example_dags.py

--- workflows/dag/example_dags.py
+++ workflows/dag/example_dags.py
@@ -73,11 +73,11 @@ with DAG(
 
     # t1, t2 and t3 are examples of tasks created by instantiating operators
     # [START basic_task]
-[docs]    t1 = BashOperator(
-        task_id="print_date",
-        bash_command="date",
+    [docs]    t1 = BashOperator(
+            task_id="print_date",
+            bash_command="date",
 
-    )
+        )
 
     t2 = BashOperator(
         task_id="sleep",
